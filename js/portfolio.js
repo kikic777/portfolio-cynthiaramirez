2ce886aa47960cb045fcd016a45e91442f128048
kikic777 <166079146+kikic777@users.noreply.github.com>
2025-08-16 21:03:05 -0700

    editing js
    trying to get the tabs to work

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,16 +1,40 @@
+
 const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
+
+console.log('Tabs:', tabs);
+console.log('Tab contents:', tabContents);
 
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.tabTarget)
+    console.log('Active Tab Target:', target);
+    
+    if (!target) {
+    console.error("Error: Tab target not found:", target);
+    return;
+    }
+    
+    // Clear active content from all tabs
     tabContents.forEach(tabContent => {
-      tabContent.classList.remove('active')
-    })
-    tabs.forEach(tab => {
-      tab.classList.remove('active')
-    })
-    tab.classList.add('active')
-    target.classList.add('active')
-  })
+
+    if (target) {
+      console.error("Error: No active content found:", target);  
+      return;
+    }
+
+    tabContent.classList.remove('active')
+     })
+
+    tabs.forEach(tab => { 
+      if (!target) {
+        console.error("Error: Tabs not removed:", target);
+        return;
+       }
+      		       
+          })
+          
+        tab.classList.add('active')
+    target.classList.add('active') 
+})
 })
